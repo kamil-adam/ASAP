368fe37380955681309e1182461eb7fbfcd5b913
Kamil Adam <kamil.adam.zabinski@gmail.com>
2016-08-26 22:12:01 +0200

    pom and classes

---
+++ YScalarWT/src/main/scala/pl/writeonly/xscalawt/examples/SWTControls.scala
@@ -0,0 +1,148 @@
+/*******************************************************************************
+ * Copyright (c) 2009 David Orme and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     David Orme - initial API and implementation
+ *******************************************************************************/
+package pl.writeonly.xscalawt
+package examples
+
+import XScalaWT._
+import Assignments._
+
+import org.eclipse.swt.layout._
+import org.eclipse.swt.widgets._
+import org.eclipse.swt.events._
+import org.eclipse.swt.SWT
+
+/*
+ * What's left?
+ * <li> Table / TableColumn / TableItem
+ * <li> TableTree, etc
+ * <li> Tree / TreeItem
+ * <li> Menu / MenuItem
+ * <li> ToolBar / ToolItem
+ * <li> functions and implicits to make events easy like already done with SelectionEvent
+ * <li> data binding!
+ * <li> JFace?
+ */
+object SWTControls {
+  def main(args : Array[String]) : Unit = {
+    
+    val window = shell("SWT Controls",
+      layout = new FillLayout(),
+      
+      tabFolder (
+        scrolledCompositeVertical (
+          tabItem("Simple controls"),
+
+          composite (
+            group("Bunch o' controls",
+              label("Hello, world"),
+              button("Push me", { e : SelectionEvent => println("Bonk!") }),
+              link("http://www.google.com")
+            ),
+            progressBar(
+              minimum = 0,
+              maximum = 50,
+              selection = 20
+            ),
+//            animatedProgress(
+//              _.start()
+//            ),
+            scaleHorizontal(
+              minimum = 0,
+              maximum = 30,
+              selection = 5
+            ),
+            scaleVertical(
+              minimum = 0,
+              maximum = 40,
+              selection = 5
+            ),
+            cLabel("Hello, CLabel"),
+//            cCombo(),
+            combo(),
+            dateTime(),
+            spinner(
+              selection = 3
+            ),
+            list(
+              _.add("One"),
+              _.add("Two"),
+              _.add("Three"),
+              selection = 1
+            ),
+            text("Edit me, please"),
+            sliderHorizontal(),
+            sliderVertical()
+          )
+        ),
+        
+        expandBar(
+          tabItem("ExpandBar"),
+          
+          composite (
+            label("Some progress meters"),
+            progressBar(
+              minimum = 0,
+              maximum = 50,
+              selection = 20
+            ),
+//            animatedProgress(
+//              _.start()
+//            ),
+            expandItem("Item 1")
+          ),
+          button("Bonk", expandItem("Item 2"))
+        ),
+        
+        composite (
+          tabItem("CoolBar"),
+          
+          coolBar(
+            layoutData = new GridData(SWT.FILL, SWT.CENTER, true, false),
+            
+            button("one", coolItem()),
+            button("two", coolItem()),
+            button("three", coolItem()),
+            composite(
+              layout = new RowLayout(),
+              button("four"),
+              button("five"),
+              button("six"),
+              button("seven"),
+              coolItem()
+            )
+          )
+        ),
+        
+        sashForm (
+          tabItem("SashForm"),
+          
+          button("Pane 1"),
+          button("Pane 2"),
+          button("Pane 3"),
+          
+          _.setWeights(Array(30, 40, 30))
+        ),
+        
+        browser (
+          tabItem("Browser"),
+          
+          _.setUrl("http://www.google.com")
+        )
+        
+//        table (
+//          tabItem("Table"),
+//        )
+      )
+    )
+    
+    runEventLoop(window)
+  }
+}
---
+++ YScalarWT/src/main/scala/pl/writeonly/xscalawt/examples/progression/TemperatureScala4.scala
@@ -0,0 +1,27 @@
+package pl.writeonly.xscalawt.examples.temp
+
+import pl.writeonly.xscalawt.XScalaWT._
+import org.eclipse.swt.widgets._
+import org.eclipse.swt.layout._
+import org.eclipse.swt.events._
+
+class Temperature4(parent: Composite, style : Int) extends Composite(parent, style) {
+  implicit def unboxText2Double(t : Text) = java.lang.Double.parseDouble(t.getText())
+  implicit def convertDouble2String(d : Double) = d.toString
+ 
+  var fahrenheit : Text = null
+  var celcius : Text = null
+ 
+  this.contains (
+    _.setLayout(new GridLayout(2, true)),
+ 
+    label("Fahrenheit"),
+    label("Celcius"),
+ 
+    text(fahrenheit = _),
+    text(celcius = _),
+ 
+    button("Fahrenheit -> Celcius", onSelection {e => celcius.text = ((5.0/9.0) * (fahrenheit - 32)) }),
+    button("Celcius -> Fahrenheit", {e : SelectionEvent => fahrenheit.text = ((9.0/5.0) * celcius + 32) })
+  )
+}
