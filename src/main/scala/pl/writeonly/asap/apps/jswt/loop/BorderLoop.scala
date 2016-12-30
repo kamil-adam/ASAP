035d279b114976076c6b75e39cd16679993d57f6
Kamil Adam <kamil.adam.zabinski@gmail.com>
2016-12-30 20:07:36 +0100

    BorderLoop

---
+++ src/main/scala/pl/writeonly/asap/apps/jswt/loop/BorderLoop.scala
@@ -0,0 +1,36 @@
+package pl.writeonly.xscalawt
+
+import pl.writeonly.jswt.scaladsl.XScalaWT.Assignments._
+import pl.writeonly.jswt.scaladsl.XScalaWT.button
+import pl.writeonly.jswt.scaladsl.XScalaWT.runEventLoop
+import pl.writeonly.jswt.scaladsl.XScalaWT.shell
+import pl.writeonly.jswt.scaladsl.XScalaWT.string2setText
+import pl.writeonly.jswt.scaladsl.layout.BorderData
+import pl.writeonly.jswt.scaladsl.YScalaWT.borderLayoutScalars
+import pl.writeonly.jswt.scaladsl.YScalaWT.textArea
+
+object BorderLoop extends App {
+  new BorderLoop().run
+}
+
+class BorderLoop  {
+  def run {
+    val window = shell(
+      "BorderLoop",
+      borderLayoutScalars(),
+      button("west", layoutData = BorderData.WEST),
+      button("east", layoutData = BorderData.EAST),
+      button("north", layoutData = BorderData.NORTH),
+      button("south", layoutData = BorderData.SOUTH),
+      textArea("center"))
+    runEventLoop(window)
+  }
+}
+
+//@Repository, @Service, @Controller
+//card
+//composite
+//controler
+//component
+//dao
+//entity
